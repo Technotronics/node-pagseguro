6e7203b1281ee24d4d230faa0b398a61847a17e6
Joaquim Neto <jsn.piata@gmail.com>
2016-04-05 22:58:39 -0300

    Atualizado versões da url comunicação com o pagseguro

--- config/config.uri.js
+++ config/config.uri.js
@@ -1,10 +1,10 @@
 module.exports = {
   payment : {
     checkout : function (config) {
-      return 'https://ws.pagseguro.uol.com.br/v3/checkout?email=' + config.email + '&token=' + config.token;
+      return 'https://ws.pagseguro.uol.com.br/v2/checkout?email=' + config.email + '&token=' + config.token;
     },
     payment : function (paymentCode) {
-      return 'https://pagseguro.uol.com.br/v3/checkout/payment.html?code=' + paymentCode;
+      return 'https://pagseguro.uol.com.br/v2/checkout/payment.html?code=' + paymentCode;
     },
     notificationConsult : function (notificationCode, config) {
       return 'https://ws.pagseguro.uol.com.br/v3/transactions/notifications/' + notificationCode + '?email=' + config.email + '&token=' + config.token;
